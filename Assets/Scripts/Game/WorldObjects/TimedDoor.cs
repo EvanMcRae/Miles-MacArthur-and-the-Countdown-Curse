aa67f13274fd8022b6f16975f1272255ee1d340a
RJWMD <113476572+RJWMD@users.noreply.github.com>
2026-07-22 19:07:32 -0400

    Added timed doors

---
+++ Assets/Scripts/Game/WorldObjects/TimedDoor.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedDoor : MonoBehaviour
+{
+    public List<DoorScheduleEntry> schedule = new List<DoorScheduleEntry>();
+
+    public Collider2D cldr;
+
+    public int warningTime = 30;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        AudioManager.OnBeat += Onbeat;
+    }
+
+    public void Onbeat(int beatNum)
+    {
+        for(int i = schedule.Count - 1; i >= 0; i--)
+        {
+            if (schedule[i].beatNum == beatNum)
+            {
+                if (schedule[i].toState == DoorState.Open)
+                    Open();
+                else
+                    Close();
+            }
+            //For animating the fill in, incremental visual changes should happen to show it is filling in soon
+            if (schedule[i].beatNum == beatNum + warningTime)
+            {
+
+            }
+            
+        }
+    }
+
+    public void Open()
+    {
+        cldr.enabled = false;
+    }
+
+    public void Close()
+    {
+        cldr.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+[System.Serializable]
+public class DoorScheduleEntry
+{
+    public int beatNum;
+    public DoorState toState;
+}
+
+public enum DoorState
+{
+    Open,
+    Closed
+}
