15fd3d90d1fc10385bb3eeaf76bd0f54cad19b89
RJWMD <113476572+RJWMD@users.noreply.github.com>
2026-07-22 15:01:52 -0400

    Basic player movement

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -1,8 +1,15 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 public class Player : MonoBehaviour
 {
+    public PlayerInput inputSettings;
+    public Tilemap ColliderTiles;
+
+    Vector2Int lastMoveDirInput = default;
+
     void Start()
     {
         
@@ -10,7 +17,42 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        HandleMovement();
+    }
+
+    public void HandleMovement()
+    {
+        Vector2 inputMove = inputSettings.actions["Move"].ReadValue<Vector2>();
+
         
+
+        Vector2Int currPosition = new Vector2Int(Mathf.FloorToInt(transform.position.x), Mathf.FloorToInt(transform.position.y));
+        Vector2Int inputMoveDir = new Vector2Int(Mathf.RoundToInt(inputMove.x), Mathf.RoundToInt(inputMove.y));
+
+        if (inputMoveDir == default)
+        {
+            lastMoveDirInput = inputMoveDir;
+            return;
+        }
+
+        
+
+        //Separate vertical movement and horizontal separately, to prevent moving diagonally without testing either side
+        if (lastMoveDirInput.y != inputMoveDir.y
+            && ColliderTiles.GetTile((Vector3Int)(currPosition + Vector2Int.up * inputMoveDir.y)) == null)
+        {
+            transform.position += new Vector3(0, inputMoveDir.y, 0);
+            currPosition += Vector2Int.up * inputMoveDir.y;
+        }
+
+        //Separate vertical movement and horizontal separately, to prevent moving diagonally without testing either side
+        if (lastMoveDirInput.x != inputMoveDir.x
+            && ColliderTiles.GetTile((Vector3Int)(currPosition + Vector2Int.right * inputMoveDir.x)) == null)
+        {
+            transform.position += new Vector3(inputMoveDir.x, 0, 0);
+        }
+
+        lastMoveDirInput = inputMoveDir;
     }
 
     void OnPause(InputValue _)
