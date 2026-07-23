a26bd216fb589552e5f033b0584a49295adba579
RJWMD <113476572+RJWMD@users.noreply.github.com>
2026-07-23 15:10:03 -0400

    Added timer to UI

---
+++ Assets/Scripts/UI/UITimer.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+
+public class UITimer : MonoBehaviour
+{
+    public int totalBeats = 120;
+    //Divide the count to make it look like less time that passes slower
+    int counterReduction = 2;
+    public TextMeshProUGUI text;
+
+    private void Start()
+    {
+        AudioManager.OnBeat += SetTimer;
+        text.text = "" + totalBeats / counterReduction;
+    }
+
+    public void SetTimer(int beatNum)
+    {
+        text.text = "" + (totalBeats - beatNum)/counterReduction;
+    }
+}
