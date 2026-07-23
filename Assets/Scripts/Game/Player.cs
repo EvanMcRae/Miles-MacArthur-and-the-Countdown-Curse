f41894b17429e2ac3a84f0b61fcedb0839298f4e
EvanMcRae <evanmmcrae@gmail.com>
2026-07-23 12:44:36 -0400

    Rotate when moving against walls
    Also some cleanup to remove needless news

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -79,13 +79,13 @@ public class Player : MonoBehaviour
             //Separate vertical movement and horizontal separately, to prevent moving diagonally without testing either side.
             if (CheckOpenTile(currPosition + Vector2Int.up * inputMoveDir.y))
             {
-                transform.position += new Vector3(0, inputMoveDir.y, 0);
-                xDirection = 0;
-                yDirection = inputMoveDir.y;
+                transform.position += Vector3.up * inputMoveDir.y;
                 currPosition += Vector2Int.up * inputMoveDir.y;
-                anim.Play("PlayerMove", 0, 0);
-                spriteRenderer.transform.rotation = Quaternion.Euler(Vector3.forward * (inputMoveDir.y > 0 ? 0 : 180));
             }
+            xDirection = 0;
+            yDirection = inputMoveDir.y;
+            spriteRenderer.transform.rotation = Quaternion.Euler(Vector3.forward * (inputMoveDir.y > 0 ? 0 : 180));
+            anim.Play("PlayerMove", 0, 0);
             lastMoveYTimer = MOVE_COOLDOWN;
         }
         else
@@ -98,12 +98,12 @@ public class Player : MonoBehaviour
             //Separate vertical movement and horizontal separately, to prevent moving diagonally without testing either side.
             if (CheckOpenTile(currPosition + Vector2Int.right * inputMoveDir.x))
             {
-                transform.position += new Vector3(inputMoveDir.x, 0, 0);
-                yDirection = 0;
-                xDirection = inputMoveDir.x;
-                anim.Play("PlayerMove", 0, 0);
-                spriteRenderer.transform.rotation = Quaternion.Euler(Vector3.forward * inputMoveDir.x * -90);
+                transform.position += Vector3.right * inputMoveDir.x;
             }
+            yDirection = 0;
+            xDirection = inputMoveDir.x;
+            spriteRenderer.transform.rotation = Quaternion.Euler(Vector3.forward * inputMoveDir.x * -90);
+            anim.Play("PlayerMove", 0, 0);
             lastMoveXTimer = MOVE_COOLDOWN;
         }
         else
@@ -144,7 +144,7 @@ public class Player : MonoBehaviour
             heldItem.isBeingHeld = true;
             heldItem.GetComponentInChildren<SpriteRenderer>().sortingOrder = 2;
             heldItem.transform.SetParent(transform, false);
-            heldItem.transform.position = new Vector2(transform.position.x, transform.position.y + .25f);
+            heldItem.transform.position = Vector2.right * transform.position.x + Vector2.up * (transform.position.y + .25f);
         }
     }
 
@@ -169,7 +169,7 @@ public class Player : MonoBehaviour
 
     public Vector2Int GetPointInFrontOfPlayer()
     {
-        return new Vector2Int(Mathf.FloorToInt(transform.position.x + xDirection), Mathf.FloorToInt(transform.position.y + yDirection));
+        return Vector2Int.right * Mathf.FloorToInt(transform.position.x + xDirection) + Vector2Int.up * Mathf.FloorToInt(transform.position.y + yDirection);
     }
 
     public Item GetItemInFrontOfPlayer()
@@ -180,7 +180,7 @@ public class Player : MonoBehaviour
         //Check tile in front of player for item.
         ////Vector2 test = Vector2.one * .5f + frontTile;
         ////Debug.DrawLine((Vector3)test, new Vector3(test.x - .3f, test.y -.3f, 0), Color.white, 5);
-        Collider2D[] cols = Physics2D.OverlapBoxAll(Vector2.one * .5f + frontTile, new Vector2(.3f, .3f), 0);
+        Collider2D[] cols = Physics2D.OverlapBoxAll(Vector2.one * .5f + frontTile, Vector2.one * .3f, 0);
 
 
         //Search for items gotten from prev method.
