3ea840aa6150b5ebe0ee3c5bdec997ca2d9daaa0
EvanMcRae <evanmmcrae@gmail.com>
2026-07-24 17:38:21 -0400

    Tab bound to pause for web builds

--- Assets/Scripts/Sounds/AudioManagerCheck.cs
+++ Assets/Scripts/Sounds/AudioManagerCheck.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AudioManagerCheck : MonoBehaviour
 {
     public GameObject audioManager;
     [SerializeField] private MusicClip firstSong;
     [SerializeField] private float startDelay = 0.5f;
+    [SerializeField] private InputActionReference _pauseAction;
 
     // Start is called before the first frame update
     void Awake()
@@ -19,11 +21,16 @@ public class AudioManagerCheck : MonoBehaviour
         {
             StartCoroutine(StartMusic());
         }
+
+        if (Utils.IsWebPlayer())
+        {
+            _pauseAction.action.ApplyBindingOverride(0, "<Keyboard>/tab");
+        }
     }
 
     private IEnumerator StartMusic()
     {
         yield return new WaitForSeconds(startDelay);
         AudioManager.instance.ChangeBGM(firstSong);
-    } 
+    }
 }
