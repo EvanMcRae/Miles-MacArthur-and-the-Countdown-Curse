471eafc9704d163f7a0152a204714a7b5f41591e
EvanMcRae <evanmmcrae@gmail.com>
2026-07-24 23:00:45 -0400

    Revise hourglass visual

--- Assets/Scripts/UI/UITimer.cs
+++ Assets/Scripts/UI/UITimer.cs
@@ -6,17 +6,19 @@ public class UITimer : MonoBehaviour
     public int totalBeats = 120;
     //Divide the count to make it look like less time that passes slower
     int counterReduction = 2;
-    public TextMeshProUGUI text;
+    public TextMeshProUGUI text, shadowText;
 
     private void Start()
     {
         AudioManager.OnBeat += SetTimer;
         text.text = "" + totalBeats / counterReduction;
+        shadowText.text = text.text;
     }
 
     public void SetTimer(int beatNum)
     {
         text.text = "" + (totalBeats - beatNum)/counterReduction;
+        shadowText.text = text.text;
 
         // Lose logic
         if ((totalBeats - beatNum) / counterReduction <= 0) 
