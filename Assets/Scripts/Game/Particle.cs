f0ef9c8007a4ed88c18b383ddc719fa69720fc80
EvanMcRae <evanmmcrae@gmail.com>
2026-07-25 17:22:29 -0400

    Setup for particle sound

--- Assets/Scripts/Game/Particle.cs
+++ Assets/Scripts/Game/Particle.cs
@@ -6,6 +6,8 @@ public class Particle : MonoBehaviour
     private int BeatsToClean;
     //how many beats left unil this bomb disapears
     private int BeatsLeftUnilClean;
+    [SerializeField] private SoundPlayer soundPlayer;
+    [SerializeField] private SoundClip sound;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,6 +15,11 @@ public class Particle : MonoBehaviour
         BeatsLeftUnilClean = BeatsToClean;
 
         AudioManager.OnBeat += Onbeat;
+
+        if (soundPlayer != null && sound != null)
+        {
+            soundPlayer.PlaySound(sound);
+        }
     }
 
     public void Onbeat(int beatNum)
